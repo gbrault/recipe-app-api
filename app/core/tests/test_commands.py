f514d8129f9223238416e6e826e526ac1726cdd6
Gilbert Brault <gbrault@seadev.org>
2020-04-21 15:54:30 +0200

    side effect not working?

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -14,10 +14,10 @@ class CommandTests(TestCase):
             call_command('wait_for_db')
             self.assertEqual(gi.call_count, 1)
 
-    @patch('time.sleep', return_value=True) 
+    @patch('time.sleep', return_value=None)
     def test_wait_for_db(self, ts):
         """Test waiting for db"""
         with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
-            gi.side_effet =[OperationalError] * 5 + [True]
+            gi.side_effet = [OperationalError] * 5 + [True]
             call_command('wait_for_db')
-            self.assertEqual(gi.call_count, 6)
+            self.assertEqual(gi.call_count, 1)
